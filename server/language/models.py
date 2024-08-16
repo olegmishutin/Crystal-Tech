4cd7e550eaf023a4d49940b60a63fb8aebe0b648
olegmishutin <omisutin646@gmail.com>
2024-08-16 15:32:18 +0300

    fixed

--- server/language/models.py
+++ server/language/models.py
@@ -9,7 +9,7 @@ class Language(models.Model):
         'py': 'Python'
     }
 
-    name = models.CharField('название', max_length=150, choices=choices, unique=True)
+    name = models.CharField('название', max_length=150, choices=choices)
     _image = models.ImageField('картинка', upload_to='languagesIcons/')
     users = models.ManyToManyField(get_user_model(), db_table='CompletedLanguage', related_name='completedLanguages',
                                    verbose_name='сдавшие')
